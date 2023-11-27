f9bdd065eb13346b0fb78bc09dfcecb02c820edc
BoboxonXolmatov <152167597+BoboxonXolmatov@users.noreply.github.com>
2023-11-27 14:13:47 +0300

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container ">
+        <p>ALl Reserved <a href="#">Boboxon</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
